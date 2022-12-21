f746db45e54637cc3e1c8016d4bf669e70d4062a
Luong Anh Tuan <tuanlase02874@gmail.com>
2022-12-21 17:25:44 +0700

    update code

--- train.py
+++ train.py
@@ -33,6 +33,7 @@ test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
 with open("metrics.txt", 'w') as outfile:
+        outfile.write("Metric of Results")
         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
         outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
